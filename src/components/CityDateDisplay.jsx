5312d5a129ab3db11334dc7799c60104861ff1a9
meltemalay <malay@nyit.edu>
2024-12-20 19:14:17 -0500

    initial commit

---
+++ src/components/CityDateDisplay.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+const cityName = {
+  textAlign: "center",
+  fontSize: "1.5rem",
+  fontWeight: "bold",
+  color: "#333",
+}
+
+function CityDateDisplay({ data }) {
+  // Convert the current date to a more readable format
+  const date = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <div className="container city-date-display">
+      <div className="location">
+        <p style={cityName}>{data.name}</p> {/* Display city name */}
+        <p>{date}</p> {/* Display formatted date */}
+      </div>
+    </div>
+  );
+}
+
+export default CityDateDisplay;
